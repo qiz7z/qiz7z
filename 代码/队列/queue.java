116b3e631328fe7638c649ed33d67f873c45bb17
liuqiqi <134282736+qiz7z@users.noreply.github.com>
2023-09-30 13:33:38 +0800

    Create queue.java

---
+++ 代码/队列/queue.java
@@ -0,0 +1,22 @@
+package 队列;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Test {
+    public static void main(String[] args) {
+        //是一个接口继承于collection
+        Queue<Integer> queue=new ArrayDeque<>();
+        queue.add(12);//超出边界会抛出异常
+        queue.offer(13);
+        /*System.out.println("queue.remove() = " + queue.remove());//从头部删除，并返回
+        System.out.println(queue);
+        System.out.println("queue.remove() = " + queue.remove());//从头部删除，并返回
+        System.out.println(queue);
+        System.out.println("queue.poll() = " + queue.poll());*/
+
+        System.out.println("queue.element() = " + queue.element());//get the first element in queue
+        Integer peek = queue.peek();
+        System.out.println("peek = " + peek);
+    }
+}

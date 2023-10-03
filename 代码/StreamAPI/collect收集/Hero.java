eaa5125108c8d91a5d99c69f11ae55e6c785e2ea
liuqiqi <134282736+qiz7z@users.noreply.github.com>
2023-10-03 16:15:25 +0800

    Create Hero.java

---
+++ 代码/StreamAPI/collect收集/Hero.java
@@ -0,0 +1,43 @@
+package StreamAPI;
+
+
+//王者荣耀
+//英雄，名字，血量
+//将所有血量值小于5000的英雄取出
+public class Hero {
+    private String name;
+    private int blood;
+
+    public Hero() {
+    }
+
+    public Hero(String name, int blood) {
+        this.name = name;
+        this.blood = blood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBlood() {
+        return blood;
+    }
+
+    public void setBlood(int blood) {
+        this.blood = blood;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + name + '\'' +
+                ", blood=" + blood +
+                '}';
+    }
+}
+

71afea2e625d0cc14c2c7fb41f800fb25a28056e
liuqiqi <134282736+qiz7z@users.noreply.github.com>
2023-10-08 10:09:17 +0800

    Create TestStudent.java

---
+++ 代码/IO流/对象序列化/反序列化/TestStudent.java
@@ -0,0 +1,25 @@
+package com.coder;
+
+import java.io.*;
+
+public class TestStudent {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //t1();
+        t2();
+    }
+    public static void t1() throws IOException{//序列化
+        Student student=new Student("apple","man",23,"china");
+        FileOutputStream fos=new FileOutputStream("e:/aa/stu.xxx");
+        ObjectOutputStream oos=new ObjectOutputStream(fos);//对象的序列化的一个流
+        oos.writeObject(student);
+        oos.close();
+    }
+    public static void t2() throws IOException, ClassNotFoundException {//反序列化
+        FileInputStream fis=new FileInputStream("e:/aa/stu.xxx");
+        ObjectInputStream ois=new ObjectInputStream(fis);
+        Object student = (Student)ois.readObject();
+        System.out.println(student);
+        ois.close();
+
+    }
+}

53cfc6444415ea2133955ba89b957de18c03ca98
liuqiqi <134282736+qiz7z@users.noreply.github.com>
2023-10-08 09:40:59 +0800

    Create Student.java

---
+++ 代码/IO流/对象序列化/序列化/Student.java
@@ -0,0 +1,52 @@
+package com.coder;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {//标识接口
+    private String name;
+    private String gender;
+    private int age;
+    private String address;
+
+    public Student() {
+    }
+
+    public Student(String name, String gender, int age, String address) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
